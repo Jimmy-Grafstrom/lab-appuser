ee7d7f2f7a5c291e925e11ec036283a782009ead
Jimmy-Grafstrom <jimmy.grafstrom@gmail.com>
2026-03-10 09:25:20 +0100

    feat: add logging in AppUserService

--- src/main/java/se/jimmy/labappuser/service/AppUserService.java
+++ src/main/java/se/jimmy/labappuser/service/AppUserService.java
@@ -8,26 +8,38 @@ import se.jimmy.labappuser.entity.AppUser;
 import se.jimmy.labappuser.repository.AppUserRepository;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @ApplicationScoped
 public class AppUserService {
+
+    private static final Logger logger = Logger.getLogger(AppUserService.class.getName());
 
     @Inject
     private AppUserRepository appUserRepository;
 
     @Transactional
     public void saveUser(AppUser appUser) {
-        appUserRepository.save(appUser);
+            appUserRepository.save(appUser);
+            logger.log(Level.INFO, "User {0} saved successfully!", appUser.getUsername());
     }
 
     public List<AppUser> getAllUsers() {
+        logger.fine("Fetching all users");
         return appUserRepository.findAll();
     }
 
     public AppUser login(String username, String password) {
-        return appUserRepository.findByUsername(username)
+        AppUser appUser = appUserRepository.findByUsername(username)
                 .filter(user -> user.getPassword().equals(password))
                 .orElse(null);
+        if (appUser != null) {
+            logger.log(Level.INFO, "User {0} logged in successfully!", appUser.getUsername());
+        } else  {
+            logger.log(Level.WARNING, "Wrong username or password!");
+        }
+        return appUser;
     }
 
 }
